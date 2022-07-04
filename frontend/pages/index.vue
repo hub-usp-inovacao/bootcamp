55652aa5315bce4e27604559d22b705caa888cce
João Daniel <jotaf.daniel@gmail.com>
2022-07-04 00:19:57 -0300

    pagina de usuario
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,35 @@
 <template>
-  <h1>Home Page</h1>
+  <main>
+    <div>
+      <label for="name">Nome</label>
+      <input id="name" type="text" v-model="name" />
+
+      <label for="email">Email</label>
+      <input id="email" type="email" v-model="email" />
+
+      <label for="cpf">CPF</label>
+      <input id="cpf" type="text" v-model="cpf" />
+    </div>
+
+    <hr />
+
+    <div>
+      <ul>
+        <li>Nome: {{ name }}</li>
+        <li>Email: {{ email }}</li>
+        <li>CPF: {{ cpf }}</li>
+      </ul>
+    </div>
+  </main>
 </template>
+
+
+<script>
+export default {
+  data: () => ({
+    name: "",
+    email: "",
+    cpf: "",
+  }),
+};
+</script>
