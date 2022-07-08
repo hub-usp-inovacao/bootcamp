4e4f0c1b6be16495b31e6a622637ff2c77d84cb9
BrizaMel <brizamel.dias@gmail.com>
2022-07-08 16:20:20 -0300

    Add new file with simple form
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,58 @@
 <template>
-  <h1>Home Page</h1>
+
+<v-container>
+  <v-card
+    class="mx-auto"
+    max-width="1000"
+    flat
+  >
+    <v-card-title>
+      Formulário
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field
+            v-model="name"
+            label="Name"
+          ></v-text-field>
+  
+          <v-text-field
+            v-model="cpf"
+            label="CPF"
+          ></v-text-field>
+  
+          <v-text-field
+            v-model="email"
+            label="Email"
+          ></v-text-field>
+      </v-form>
+
+      <v-list-item>
+        {{ name }}
+      </v-list-item>
+
+       <v-list-item>
+        {{ cpf }}
+      </v-list-item>
+
+       <v-list-item>
+        {{ email }}
+      </v-list-item>
+
+    </v-card-text>
+  </v-card>
+
+   
+
+</v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+      name: '',
+      cpf: '',
+      email: '',
+    }),
+};
+</script>
