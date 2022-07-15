922818e4c4389ebd404add3327ca2ee0b3671790
mariliatd <mariliatd@gmail.com>
2022-07-15 17:44:15 -0300

    Add inputEmail component
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ frontend/components/InputEmail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="email"
+            label="email"
+            @blur="validateEmail"
+        ></v-text-field>
+        <p v-if="emailError.length"> {{ emailError }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        email: '',
+        emailError: '',
+    }),
+
+    watch: {
+        email: function() {
+            this.$emit("input", this.email)
+        }
+    },
+
+    methods: {
+        validateEmail: function () {
+            if (this.email.includes('@'))
+                this.emailError = ""
+            else
+                this.emailError = "Email inválido"
+        }
+    }
+}
+</script>
