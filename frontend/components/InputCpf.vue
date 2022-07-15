809f683babe2084442038b8b36f6a7b980266cc3
mariliatd <mariliatd@gmail.com>
2022-07-15 17:43:06 -0300

    Add inputCpf component
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ frontend/components/InputCpf.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <v-text-field
+            v-model="cpf"
+            label="CPF"
+            @blur="validateCPF"
+        ></v-text-field>
+        <p v-if="cpfError.length"> {{ cpfError }} </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        cpf: '',
+        cpfError: '',
+    }),
+
+    watch: {
+        cpf: function() {
+            this.$emit("input", this.cpf)
+        }
+    },
+
+    methods: {
+        validateCPF: function () {
+        let cleanedCpf = this.cpf.replaceAll(/\D/g, '')
+        if (cleanedCpf.length != 11)
+            this.cpfError = "CPF inválido"
+        else
+            this.cpfError = ""
+        },
+    }
+}
+</script>
