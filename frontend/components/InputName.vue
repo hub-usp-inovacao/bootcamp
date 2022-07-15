8be7578deb8111bc5544ead47a6c408be43d015a
mariliatd <mariliatd@gmail.com>
2022-07-15 17:39:38 -0300

    Add inputName component
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ frontend/components/InputName.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-text-field
+        v-model="name"
+        label="Name"
+    ></v-text-field>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        name: '',
+    }),
+
+    watch: {
+        name: function() {
+            this.$emit("input", this.name)
+        }
+    }
+}
+</script>
