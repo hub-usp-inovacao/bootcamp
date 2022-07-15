f7a454751d4495b6946a9fd31bcb69c0b4b88ecb
mariliatd <mariliatd@gmail.com>
2022-07-15 17:45:33 -0300

    Add Display component
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ frontend/components/Display.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card-text>
+      <v-list-item>
+        {{ name }}
+      </v-list-item>
+
+       <v-list-item>
+        {{ cpf }}
+      </v-list-item>
+
+       <v-list-item>
+        {{ email }}
+      </v-list-item>
+    </v-card-text>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        cpf: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+    }
+
+    
+}
+</script>
