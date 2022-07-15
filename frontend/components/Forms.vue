944e839de820b0bd96d97af47348973ab368f9ec
mariliatd <mariliatd@gmail.com>
2022-07-15 17:44:54 -0300

    Add Forms component
    Co-authored-by: Briza Mel <brizamel.dias@gmail.com>
    Co-authored-by: João Daniel <jotaf.daniel@gmail.com>
    Co-authored-by: Luiz <luizcarlosdk@hotmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

---
+++ frontend/components/Forms.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <v-card-title>
+            Formulário
+        </v-card-title>
+        <v-card-text>
+            <v-form>
+                <InputName @input="name=$event"/>
+                <InputCpf @input="cpf=$event"/>         
+                <InputEmail @input="email=$event"/>
+            </v-form>
+        </v-card-text>
+    </div>
+</template>
+
+<script>
+
+import InputName from "@/components/InputName.vue"
+import InputCpf from "@/components/InputCpf.vue"
+import InputEmail from "@/components/InputEmail.vue"
+
+export default {
+    components: {
+        InputName,
+        InputCpf,
+        InputEmail,
+    },
+
+    data: () => ({
+      name: '',
+      cpf: '',
+      email: '',
+    }),
+
+    watch: {
+        name: function() {
+            this.$emit("inputName", this.name)
+        },
+        cpf: function() {
+            this.$emit("inputCpf", this.cpf)
+        },
+        email: function() {
+            this.$emit("inputEmail", this.email)
+        },
+    },
+
+}
+
+
+
+</script>
