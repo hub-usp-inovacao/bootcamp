f72293b7a8923cb9adffd0fb1ca1d307e9dfd44d
BrizaMel <brizamel.dias@gmail.com>
2022-07-29 18:05:04 -0300

    Add new routes
    Signed-off-by: BrizaMel <brizamel.dias@gmail.com>
    Co-authored-by: Marília Dicezare <mariliatd@gmail.com>

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   get "/", to: "application#index"
+  #get "/users/:id", to: "users#read"
+  get "/users/readAll", to: "users#readAll"
+  post "/users", to: "users#create"
 end
