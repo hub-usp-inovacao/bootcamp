c5315db54c617d162c01bda58b89b2aad0b8a049
Clara Yuki <cyseolar@gmail.com>
2023-02-27 16:13:17 -0300

    Add input first components
    Co-authored-by: Clara Yuki <cyseolar@gmail.com>
    Co-authored-by: José Lucas Silva Mayer <josemayer@usp.br>

---
+++ frontend/components/InputEmail.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-text-field
+		v-model="field.email"
+		label="E-mail"
+		:rules="emailRules"
+		required
+	></v-text-field>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            emailRules: [
+                v => !!v || 'O e-mail é obrigatório',
+                v => /.+@.+\..+/.test(v) || 'E-mail inválido',
+            ],
+        }),
+
+        props: {
+            field: {
+                type: Object,
+                required: true
+            }
+        },
+    }
+</script>
---
+++ frontend/components/InputText.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-text-field
+		v-model="field.name"
+		label="Nome"
+		:rules="[v => !!v || 'O nome é obrigatório']"
+		required
+	></v-text-field>
+</template>
+
+<script>
+    export default {
+        props: {
+            field: {
+                type: Object,
+                required: true
+            }
+        }
+    }
+</script>
